967515484309339d7cbafe9f5c0ae796bce8f120
Vedran Novak <vednovak@gmail.com>
2026-03-28 11:23:07 +0100

    refactor

---
+++ arrays/decode_string.py
@@ -0,0 +1,9 @@
+# def decode_string(input, result):
+#     num, result = 
+#     for c in input:
+#         if isinstance(c, int):
+            
+   
+# print("3[a2[c]]2[b]")
+
+# isinstance(4, int)
